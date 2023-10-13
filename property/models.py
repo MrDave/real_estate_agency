0c0864838c0f6f2b535f72c010b9b52296db8f27
MrDave <davidschnoll@gmail.com>
2023-10-13 16:31:10 +0300

    Fix obligatory likes

--- property/models.py
+++ property/models.py
@@ -50,7 +50,7 @@ class Flat(models.Model):
         blank=True,
         db_index=True)
     new_building = models.BooleanField(verbose_name='Новостройка')
-    liked_by = models.ManyToManyField(User, verbose_name='Кто лайкнул', related_name='liked_flats')
+    liked_by = models.ManyToManyField(User, verbose_name='Кто лайкнул', related_name='liked_flats', blank=True)
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
