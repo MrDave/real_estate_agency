6f4c1f4652d363351b4513540c86cb3eba9dbc03
MrDave <davidschnoll@gmail.com>
2023-10-19 17:37:41 +0300

    Fix broken migration

--- property/migrations/0004_auto_20231011_1817.py
+++ property/migrations/0004_auto_20231011_1817.py
@@ -7,6 +7,7 @@ def mark_building(apps, schema_editor):
     Flat = apps.get_model("property", "Flat")
 
     Flat.objects.filter(construction_year__gte=2015).update(new_building=True)
+    Flat.objects.filter(construction_year__lt=2015).update(new_building=False)
 
 
 class Migration(migrations.Migration):
